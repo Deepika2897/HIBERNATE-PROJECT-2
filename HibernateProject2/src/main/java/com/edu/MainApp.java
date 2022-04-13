fe32005e699c06e912e30a7c5241e0f3a6cf47c9
vijay <vijay@DESKTOP-6315K2E>
2022-04-13 18:16:53 +0530

    project using hibernate framework

---
+++ HibernateProject2/src/main/java/com/edu/MainApp.java
@@ -0,0 +1,34 @@
+package com.edu;
+
+
+		import org.hibernate.Session;
+		import org.hibernate.SessionFactory;
+		import org.hibernate.Transaction;
+		import org.hibernate.cfg.Configuration;
+		import org.hibernate.service.ServiceRegistry;
+		import org.hibernate.service.ServiceRegistryBuilder;
+
+		public class MainApp {
+
+			public static void main(String[] args) {
+				Employee eob=new Employee();
+				eob.setEid(1);
+				eob.setEname("Bismi");
+				eob.setEsalary(56767.65);
+		        Configuration conn=new Configuration().configure().addAnnotatedClass(Employee.class);	
+		        ServiceRegistry reg=new ServiceRegistryBuilder().applySettings(conn.getProperties()).buildServiceRegistry();
+		        SessionFactory sf=conn.buildSessionFactory(reg);
+		        Session sess=sf.openSession();
+		       Transaction tx=sess.beginTransaction();
+		        
+		        sess.save(eob);
+		        tx.commit();
+		        		
+			}
+
+		
+
+
+	}
+
+
